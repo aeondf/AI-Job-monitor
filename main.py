24935e8029297e1ba2cb173c6700d829494bdc30
Daniil Fedorov <daniilfedorov@MacBook-Air-Daniil-3.local>
2026-06-06 12:08:37 +0300

    first REST endpoint

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-def main():
-    print("Hello from ai-job-monitor!")
+from fastapi import FastAPI
 
+app = FastAPI()
 
-if __name__ == "__main__":
-    main()
+@app.get("/sources")
+def get_sources():
+    return ["headhunter", "LinkedIn", "HubrCareer"]
