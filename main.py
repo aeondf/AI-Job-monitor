c3521161240db9b834cbcf696e5d1af9c5591852
Daniil Fedorov <daniilfedorov@MacBook-Air-Daniil-3.local>
2026-06-06 12:31:49 +0300

    first post request

--- main.py
+++ main.py
@@ -1,7 +1,19 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+
+
+class SearchesRequest(BaseModel):
+    text: str
+
 
 app = FastAPI()
 
+
+@app.post("/search")
+def search(request: SearchesRequest) -> dict[str, str]:
+    return {"search_id": "123"}
+
+
 @app.get("/sources")
-def get_sources():
+def get_sources() -> list[str]:
     return ["headhunter", "LinkedIn", "HubrCareer"]
